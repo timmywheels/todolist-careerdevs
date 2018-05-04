9980dfcc299e947d69897c61bbeb10cea557271e
Tim Wheeler <tim@timwheeler.com>
2018-05-04 00:58:08 +0000

    Version 4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Todo List App
 
-//Version 3 Requirements
+//Version 4 Requirements
 
 var todoList = {
 	
@@ -9,23 +9,37 @@ var todoList = {
 	
 	//create display todos method
 	displayTodos: function(){
-		console.log(`My Todos: ${this.todos}`);
+		console.log('My Todos:', this.todos);
 	},
 	
-	//create add todo method
-	addTodo: function(todo){
-		this.todos.push(todo);
+	//todoList.addTodo() should add objects
+	addTodo: function(todoText){
+		this.todos.push({
+			todoText: todoText,
+			completed: false
+		});
+		this.displayTodos();
 	},
 	
-	//create change todo method
-	changeTodo: function(position, newValue){
-		this.todos[position] = newValue;
+	//todoList.changeTodo() should change todoText property
+	changeTodo: function(position, todoText){
+		this.todos[position].todoText = todoText;
+		this.displayTodos();
 	},
 	
 	//create delete todo method
 	deleteTodo: function(position){
 		this.todos.splice(position, 1);
+		this.displayTodos();
+	},
+	
+	//todoList.toggleCompleted() should change the completed property
+	toggleCompleted: function(position){
+		var todo = this.todos[position];
+		todo.completed = !todo.completed;
+		this.displayTodos();
 	}
+	
 	
 }
 
